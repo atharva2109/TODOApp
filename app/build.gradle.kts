6ad725f39c91e96fc873cbc523e1d1e6d8951189
mkenchappajunjanna1 <mkenchappajunjanna1@sheffield.ac.uk>
2023-10-27 11:20:18 +0100

    changed the compile and target to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.team2.todo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.team2.todo"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
