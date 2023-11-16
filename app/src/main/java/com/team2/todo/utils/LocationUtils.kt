f580d37ec9e2694569062b57fb9b6269fefcc5a3
Vivek Shravan Tate <vivektate.developer@gmail.com>
2023-11-16 14:25:32 +0000

    Optimised location utils code and added an pop up, if location permission not granted.

--- app/src/main/java/com/team2/todo/utils/LocationUtils.kt
+++ app/src/main/java/com/team2/todo/utils/LocationUtils.kt
@@ -1,6 +1,7 @@
 package com.team2.todo.utils
 
 import android.Manifest
+import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
@@ -15,74 +16,126 @@ import androidx.core.app.ActivityCompat
 object LocationUtils {
 
     private lateinit var context: Context
+    private lateinit var activity: Activity
 
     private var currentLocation: Location? = null
     private lateinit var locationManager: LocationManager
 
-    fun init(context: Context) {
+    fun init(context: Context, activity: Activity) {
         this.context = context
+        this.activity = activity
     }
 
     fun getCurrentLocation(callback: (Location) -> Unit) {
-        fetchCurrentLocation() { location: Location ->
-            callback(location)
+        fetchCurrentLocation {
+            callback(it)
         }
     }
 
+
     private fun fetchCurrentLocation(callback: (Location) -> Unit) {
+
         locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
         val hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
         val hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+        if (hasGps || hasNetwork) {
+            if (hasGps && !hasNetwork) {
+                fetchLocationViaGPS {
+                    callback(it)
+                }
+            } else if (!hasGps && hasNetwork) {
+                fetchLocationViaNetwork {
+                    callback(it)
+                }
+            } else if (hasGps && hasNetwork) {
+                fetchLocationViaGPS { gpsLocation: Location ->
+                    fetchLocationViaNetwork {networkLocation: Location ->
+                        if (gpsLocation!= null && networkLocation != null) {
+                            if (gpsLocation.accuracy > networkLocation.accuracy) {
+                                callback(gpsLocation)
+                            } else {
+                                callback(networkLocation)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun fetchLocationViaGPS(callback: (Location) -> Unit) {
+
         val gpsLocationListener: LocationListener = object : LocationListener {
             override fun onLocationChanged(location: Location) {
                 callback(location!!)
                 locationManager.removeUpdates(this)
             }
-
             override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
             override fun onProviderEnabled(provider: String) {}
             override fun onProviderDisabled(provider: String) {}
         }
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                2
+            )
+        }
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            5000,
+            0F,
+            gpsLocationListener
+        )
+    }
+
+    private fun fetchLocationViaNetwork(callback: (Location) -> Unit) {
 
         val networkLocationListener: LocationListener = object : LocationListener {
             override fun onLocationChanged(location: Location) {
                 callback(location!!)
                 locationManager.removeUpdates(this)
             }
-
             override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
             override fun onProviderEnabled(provider: String) {}
             override fun onProviderDisabled(provider: String) {}
         }
-        if (hasGps || hasNetwork) {
-            if (hasGps) {
-                if (ActivityCompat.checkSelfPermission(
-                        context,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        context,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // If permission is not granted
-                }
-                locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    5000,
-                    0F,
-                    gpsLocationListener
-                )
-            }
-            if (hasNetwork) {
-                locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER,
-                    5000,
-                    0F,
-                    networkLocationListener
-                )
-            }
 
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                2
+            )
         }
+        locationManager.requestLocationUpdates(
+            LocationManager.NETWORK_PROVIDER,
+            5000,
+            0F,
+            networkLocationListener
+        )
     }
 }
