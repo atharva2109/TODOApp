314469b26f19a5f3bb7a959d652b73f9b17bcce5
Sagar Honnenahalli somashekhar <shonnenahallisomashekhar1@sheffield.ac.uk>
2023-12-03 16:15:23 +0000

    added countdowntimer

---
+++ app/src/main/java/com/team2/todo/common_ui_components/CountdownTimerForDueDate.kt
@@ -0,0 +1,30 @@
+package com.team2.todo.common_ui_components
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import kotlinx.coroutines.delay
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+@Composable
+fun CountdownTimerForDueDate(
+    dueDateTime: LocalDateTime,
+): String {
+    val currentDateTime = LocalDateTime.now()
+    val timeDifferenceInMillis = ChronoUnit.MILLIS.between(currentDateTime, dueDateTime)
+    val remainingTime =
+        remember { mutableLongStateOf(timeDifferenceInMillis) }
+
+    LaunchedEffect(remainingTime) {
+        while (remainingTime.value > 0) {
+            delay(1000L)
+            remainingTime.value -= 1000L
+        }
+    }
+    val daysRemaining = remainingTime.value / (1000 * 60 * 60 * 24)
+    val hoursRemaining = (remainingTime.value % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    val minutesRemaining = (remainingTime.value % (1000 * 60 * 60)) / (1000 * 60)
+    val secondsRemaining = (remainingTime.value % (1000 * 60)) / 1000
+    return "$daysRemaining d : $hoursRemaining h : $minutesRemaining m : $secondsRemaining s"
+}
--- app/src/main/java/com/team2/todo/screens/details_page/DetailsPage.kt
+++ app/src/main/java/com/team2/todo/screens/details_page/DetailsPage.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -169,7 +170,8 @@ fun DetailsPage(todoId: Long) {
                             Box(
                                 Modifier
                                     .fillMaxWidth()
-                                    .size(150.dp)
+                                    .height(300.dp)
+
                             ) {
                                 collecetedImages.map { bitmap -> bitmap.image }
                                     ?.let { it1 -> ImageLoader(bitmapList = it1) }
