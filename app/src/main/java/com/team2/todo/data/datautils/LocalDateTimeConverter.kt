f2986f15ff6aa713971520255b5e8a6bd2222b74
Sagar Honnenahalli somashekhar <shonnenahallisomashekhar1@sheffield.ac.uk>
2023-12-05 22:15:54 +0000

    removed seconds from datetime

--- app/src/main/java/com/team2/todo/data/datautils/LocalDateTimeConverter.kt
+++ app/src/main/java/com/team2/todo/data/datautils/LocalDateTimeConverter.kt
@@ -2,19 +2,14 @@ package com.team2.todo.data.datautils
 
 import androidx.room.TypeConverter
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.time.temporal.ChronoUnit
 
 class LocalDateTimeConverter {
 
-    /* Converts Localdatetime Object to String for saving datetime inside room db */
     @TypeConverter
     fun timeStampToString(dateTime: LocalDateTime): String {
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")
-        return dateTime.format(formatter)
+        return dateTime.toString()
     }
 
-    /* Localdatetime object is obtained from datetime string  */
     @TypeConverter
     fun stringToTimeStamp(dateTimeString: String): LocalDateTime {
         return LocalDateTime.parse(dateTimeString)
